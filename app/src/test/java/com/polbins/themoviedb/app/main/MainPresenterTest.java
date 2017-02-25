77bfb983d0beed17526779329e95992671912f9e
polbins <pvintal+git@gmail.com>
2017-02-25 16:38:03 +0800

    add Main Tests, NOK cases (all passed)

--- app/src/test/java/com/polbins/themoviedb/app/main/MainPresenterTest.java
+++ app/src/test/java/com/polbins/themoviedb/app/main/MainPresenterTest.java
@@ -15,6 +15,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.List;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.refEq;
 import static org.mockito.Mockito.verify;
@@ -32,6 +33,7 @@ public class MainPresenterTest {
 
     private MainPresenter presenter;
 
+    private ApiService.SortBy defaultApiSortBy = ApiService.SortBy.RELEASE_DATE_DESCENDING;
     private List<Movie> moviesFirstPage;
     private List<Movie> moviesSecondPage;
 
@@ -48,10 +50,15 @@ public class MainPresenterTest {
         this.moviesSecondPage = moviesSecondPage.movies;
 
         // Mock API Calls
-        when(apiService.getMovies(ApiService.SortBy.RELEASE_DATE_DESCENDING, 1))
+        when(apiService.getMovies(defaultApiSortBy, 1))
                 .thenReturn(RetrofitTestUtil.createCall(moviesFirstPage));
-        when(apiService.getMovies(ApiService.SortBy.RELEASE_DATE_DESCENDING, 2))
+        when(apiService.getMovies(defaultApiSortBy, 2))
                 .thenReturn(RetrofitTestUtil.createCall(moviesSecondPage));
+    }
+
+    private void makeGetMoviesFail() {
+        when(apiService.getMovies(any(ApiService.SortBy.class), anyInt()))
+                .thenReturn(RetrofitTestUtil.createCall(500, new Movies()));
     }
 
     @Test
@@ -77,6 +84,34 @@ public class MainPresenterTest {
         // make use of PTR for loading more content
         verify(view).showLoading(true);
         verify(view).showContent(moviesSecondPage, false);
+    }
+
+    @Test
+    public void onStartLoadsContentFail() {
+        makeGetMoviesFail();
+        presenter.start();
+
+        verify(view).showLoading(false);
+        verify(view).showError();
+    }
+
+    @Test
+    public void onPullToRefreshRefreshesContentFail() {
+        makeGetMoviesFail();
+        presenter.onPullToRefresh();
+
+        verify(view).showLoading(true);
+        verify(view).showError();
+    }
+
+    @Test
+    public void onScrollToBottomLoadsMoreContentFail() {
+        makeGetMoviesFail();
+        presenter.onScrollToBottom();
+
+        // make use of PTR for loading more content
+        verify(view).showLoading(true);
+        verify(view).showError();
     }
 
 }
