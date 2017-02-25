d07147e32b8de7ee01e92c2b91d4789f0b171ea1
polbins <pvintal+git@gmail.com>
2017-02-25 14:34:40 +0800

    add MainActivity

---
+++ app/src/main/java/com/polbins/themoviedb/app/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.polbins.themoviedb.app.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.polbins.themoviedb.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
