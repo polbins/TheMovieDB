04e2ecb5583ab03a5482d1e4d2c8301c99e9fb75
polbins <pvintal+git@gmail.com>
2017-02-25 16:29:22 +0800

    add AuthenticationInterceptor for adding “api_key” to all requests

--- app/src/main/java/com/polbins/themoviedb/api/ApiModule.java
+++ app/src/main/java/com/polbins/themoviedb/api/ApiModule.java
@@ -4,13 +4,18 @@ import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.polbins.themoviedb.api.Converter.EnumConverterFactory;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
+import okhttp3.HttpUrl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
@@ -37,12 +42,36 @@ public class ApiModule {
 
     @Provides
     @Singleton
-    OkHttpClient provideOkHttpClient(StethoInterceptor stethoInterceptor) {
+    Interceptor provideAuthenticationInterceptor() {
+        return new Interceptor() {
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request original = chain.request();
+                HttpUrl originalHttpUrl = original.url();
+
+                HttpUrl url = originalHttpUrl.newBuilder()
+                        .addQueryParameter("api_key", apiKey)
+                        .build();
+
+                Request.Builder requestBuilder = original.newBuilder()
+                        .url(url);
+
+                Request request = requestBuilder.build();
+                return chain.proceed(request);
+            }
+        };
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(StethoInterceptor stethoInterceptor,
+                                     Interceptor authenticationInterceptor) {
         HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BASIC);
 
         return new OkHttpClient.Builder()
                 .addNetworkInterceptor(stethoInterceptor)
+                .addInterceptor(authenticationInterceptor)
                 .addInterceptor(loggingInterceptor)
                 .connectTimeout(60, TimeUnit.SECONDS)
                 .writeTimeout(60, TimeUnit.SECONDS)
