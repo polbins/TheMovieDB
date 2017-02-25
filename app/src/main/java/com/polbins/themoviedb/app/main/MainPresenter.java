41cb7b635274e4c808bfd53c292aa6a2f8cd0efb
polbins <pvintal+git@gmail.com>
2017-02-25 16:29:52 +0800

    pass Main Tests, OK cases

--- app/src/main/java/com/polbins/themoviedb/app/main/MainPresenter.java
+++ app/src/main/java/com/polbins/themoviedb/app/main/MainPresenter.java
@@ -1,8 +1,13 @@
 package com.polbins.themoviedb.app.main;
 
 import com.polbins.themoviedb.api.ApiService;
+import com.polbins.themoviedb.api.model.Movies;
 
 import javax.inject.Inject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by polbins on 25/02/2017.
@@ -11,6 +16,8 @@ import javax.inject.Inject;
 public class MainPresenter implements MainContract.Presenter {
     private MainContract.View view;
     private ApiService apiService;
+
+    private int page = 1;
 
     @Inject
     MainPresenter(MainContract.View view, ApiService apiService) {
@@ -21,16 +28,40 @@ public class MainPresenter implements MainContract.Presenter {
     @Override
     public void start() {
         view.showLoading(false);
+        getMovies(true);
     }
 
     @Override
     public void onPullToRefresh() {
-
+        page = 1; // reset
+        view.showLoading(true);
+        getMovies(true);
     }
 
     @Override
     public void onScrollToBottom() {
+        page++;
+        view.showLoading(true);
+        getMovies(false);
+    }
 
+    private void getMovies(final boolean isRefresh) {
+        Call<Movies> call = apiService.getMovies(ApiService.SortBy.RELEASE_DATE_DESCENDING, page);
+        call.enqueue(new Callback<Movies>() {
+            @Override
+            public void onResponse(Call<Movies> call, Response<Movies> response) {
+                if (response.isSuccessful()) {
+                    view.showContent(response.body().movies, isRefresh);
+                } else {
+                    view.showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Movies> call, Throwable t) {
+                view.showError();
+            }
+        });
     }
 
     @Override
