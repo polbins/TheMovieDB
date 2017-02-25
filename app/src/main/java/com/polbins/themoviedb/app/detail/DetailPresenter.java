ed591e2c744a1a7f6ae982b0eb489d2914c1d84a
polbins <pvintal+git@gmail.com>
2017-02-25 22:25:34 +0800

    make Detail Tests pass

--- app/src/main/java/com/polbins/themoviedb/app/detail/DetailPresenter.java
+++ app/src/main/java/com/polbins/themoviedb/app/detail/DetailPresenter.java
@@ -2,6 +2,7 @@ package com.polbins.themoviedb.app.detail;
 
 import com.polbins.themoviedb.api.ApiService;
 import com.polbins.themoviedb.api.model.Configuration;
+import com.polbins.themoviedb.api.model.Movie;
 
 import javax.inject.Inject;
 
@@ -25,7 +26,44 @@ public class DetailPresenter implements DetailContract.Presenter {
 
     @Override
     public void start(int movieId) {
-        
+        view.showLoading();
+        getMovie(movieId);
+        getConfiguration();
+    }
+
+    private void getConfiguration() {
+        Call<Configuration> call = apiService.getConfiguration();
+        call.enqueue(new Callback<Configuration>() {
+            @Override
+            public void onResponse(Call<Configuration> call, Response<Configuration> response) {
+                if (response.isSuccessful()) {
+                    view.onConfigurationSet(response.body().images);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Configuration> call, Throwable t) {
+            }
+        });
+    }
+
+    private void getMovie(int movieId) {
+        Call<Movie> call = apiService.getMovie(movieId);
+        call.enqueue(new Callback<Movie>() {
+            @Override
+            public void onResponse(Call<Movie> call, Response<Movie> response) {
+                if (response.isSuccessful()) {
+                    view.showContent(response.body());
+                } else {
+                    view.showError();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Movie> call, Throwable t) {
+                view.showError();
+            }
+        });
     }
 
     @Override
